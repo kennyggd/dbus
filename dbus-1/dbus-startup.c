441e4fbdc073caf4a5e5a9f5d90e890fdc6a1926
gag1pg <guodong.gan@my.bosch.com>
2022-05-11 06:08:18 +0000

    added startup benchmarking app

---
+++ dbus-1/dbus-startup.c
@@ -0,0 +1,45 @@
+/*
+ * Example low-level D-Bus code.
+ * Written by Matthew Johnson <dbus@matthew.ath.cx>
+ *
+ * This code has been released into the Public Domain.
+ * You may do whatever you like with it.
+ */
+#include <dbus/dbus.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+   DBusConnection* conn;
+   DBusError err;
+   int ret;
+
+   // initialise the error value
+   dbus_error_init(&err);
+
+   // connect to the DBUS system bus, and check for errors
+   conn = dbus_bus_get(DBUS_BUS_SYSTEM, &err);
+   if (dbus_error_is_set(&err)) { 
+      fprintf(stderr, "Connection Error (%s)\n", err.message); 
+      dbus_error_free(&err); 
+   }
+   if (NULL == conn) { 
+      return 1; 
+   }
+
+   // register our name on the bus, and check for errors
+   ret = dbus_bus_request_name(conn, "test.signal.source", DBUS_NAME_FLAG_REPLACE_EXISTING , &err);
+   if (dbus_error_is_set(&err)) { 
+      fprintf(stderr, "Name Error (%s)\n", err.message); 
+      dbus_error_free(&err); 
+   }
+   if (DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER != ret) { 
+      return 1;
+   }
+
+   while(1);
+}
